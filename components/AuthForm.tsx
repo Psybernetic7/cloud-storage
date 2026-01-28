627dcf657338996db2428ce0848d66325df52a70
Psybernetic7 <senjishnu.js@gmail.com>
2026-01-28 03:14:28 +0200

    Added OTP feature for sign-up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,7 +18,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
-// import OtpModal from "@/components/OTPModal";
+import OtpModal from "@/components/OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -154,9 +154,9 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
 
-      {/* {accountId && (
+      {accountId && (
         <OtpModal email={form.getValues("email")} accountId={accountId} />
-      )} */}
+      )}
     </>
   );
 };
