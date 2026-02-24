c03c86f0dba57b2543b0c7703b27af4b11bb2cf8
Psybernetic7 <senjishnu.js@gmail.com>
2026-02-24 16:36:31 +0200

    Fix deployment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -91,17 +91,21 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  const { databases, account } = await createSessionClient();
-  const result = await account.get();
-  const user = await databases.listDocuments(
-    appwriteConf.databaseId,
-    appwriteConf.usersCollectionId,
-    [Query.equal("accountId", result.$id)],
-  );
+  try {
+    const { databases, account } = await createSessionClient();
+    const result = await account.get();
+    const user = await databases.listDocuments(
+      appwriteConf.databaseId,
+      appwriteConf.usersCollectionId,
+      [Query.equal("accountId", result.$id)],
+    );
 
-  if (user.total <= 0) return null;
+    if (user.total <= 0) return null;
 
-  return parseStringify(user.documents[0]);
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const signOutUser = async () => {
